e13a18129a9803b7a26fa01d169ec13fd7e3270d
WilsonSue <113160128+WilsonSue@users.noreply.github.com>
2024-03-12 16:21:12 -0700

    Add files via upload

---
+++ include/client.h
@@ -0,0 +1,17 @@
+#ifndef COMP4981A2_CLIENT_H
+#define COMP4981A2_CLIENT_H
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+#define UINT16_MAX 65535
+#define BASE_TEN 10
+
+#endif    // COMP4981A2_CLIENT_H
